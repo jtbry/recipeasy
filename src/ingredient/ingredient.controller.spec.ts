313c6c5f359afe89ced3975486321baf0182b607
Justin Bryant <justintbry@gmail.com>
2024-01-16 11:28:24 -0500

    refactor: ingredient test now use jest.spyOn

--- src/ingredient/ingredient.controller.spec.ts
+++ src/ingredient/ingredient.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { IngredientController } from './ingredient.controller';
 import { Ingredient } from './ingredient.entity';
 import { IngredientService } from './ingredient.service';
@@ -8,23 +10,19 @@ Object.assign(testIngredient, { id: 1, name: 'Test Ingredient' });
 
 describe('IngredientController', () => {
   let controller: IngredientController;
+  let service: IngredientService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [IngredientController],
       providers: [
-        {
-          provide: IngredientService,
-          useValue: {
-            findOne: jest.fn().mockResolvedValue(testIngredient),
-            createOne: jest.fn().mockResolvedValue(testIngredient),
-            searchByName: jest.fn().mockResolvedValue([testIngredient]),
-          },
-        },
+        IngredientService,
+        { provide: getRepositoryToken(Ingredient), useClass: Repository },
       ],
     }).compile();
 
     controller = module.get<IngredientController>(IngredientController);
+    service = module.get<IngredientService>(IngredientService);
   });
 
   it('should be defined', () => {
@@ -32,16 +30,19 @@ describe('IngredientController', () => {
   });
 
   it('should find one ingredient', async () => {
+    jest.spyOn(service, 'findOne').mockResolvedValue(testIngredient);
     expect(await controller.findOne(1)).toEqual(testIngredient);
   });
 
   it('should create an ingredient', async () => {
+    jest.spyOn(service, 'createOne').mockResolvedValue(testIngredient);
     const ingredient = new Ingredient();
     Object.assign(ingredient, testIngredient);
     expect(await controller.createOne(ingredient)).toEqual(testIngredient);
   });
 
   it('should search for ingredients by name', async () => {
+    jest.spyOn(service, 'searchByName').mockResolvedValue([testIngredient]);
     expect(await controller.searchByName('Test')).toEqual([testIngredient]);
   });
 });
